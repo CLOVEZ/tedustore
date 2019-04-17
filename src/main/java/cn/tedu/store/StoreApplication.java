e14eb31cf1310194b9fc3c4c6e0c617537f8c08b
mischenxt <799597505@qq.com>
2019-04-17 19:55:41 +0800

    day07

--- src/main/java/cn/tedu/store/StoreApplication.java
+++ src/main/java/cn/tedu/store/StoreApplication.java
@@ -3,13 +3,30 @@ package cn.tedu.store;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
+
+import javax.servlet.MultipartConfigElement;
 
 @SpringBootApplication
 @MapperScan("cn.tedu.store.mapper")
+@Configuration
 public class StoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StoreApplication.class, args);
+	}
+	@Bean
+	public MultipartConfigElement getMultipartConfig(){
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+
+		DataSize maxSize = DataSize.ofMegabytes(100L);
+		factory.setMaxFileSize(maxSize);
+		factory.setMaxRequestSize(maxSize);
+		MultipartConfigElement element = factory.createMultipartConfig();
+		return element;
 	}
 
 }
